5f277ced6b91d37eb889943591875c61096c5987
Huda Arshad <hudaarshad2004@gmail.com>
2026-07-23 15:44:00 +0500

    added wallet and executon modules where dropdown tell details

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,9 @@ import DashboardLayout from './layouts/DashboardLayout';
 import Dashboard from './pages/Dashboard';
 import Strategies from './pages/Strategies';
 import StrategyDetails from './pages/StrategyDetails';
+import Wallets from './pages/Wallets';
+import Deployment from './pages/Deployment';
+import ExecutionDetails from './pages/ExecutionDetails';
 
 function App() {
   return (
@@ -15,6 +18,9 @@ function App() {
             <Route path="/" element={<Dashboard />} />
             <Route path="/strategies" element={<Strategies />} />
             <Route path="/strategies/:id" element={<StrategyDetails />} />
+            <Route path="/wallets" element={<Wallets />} />
+            <Route path="/deployment" element={<Deployment />} />
+            <Route path="/deployment/:id" element={<ExecutionDetails />} />
           </Route>
         </Routes>
       </BrowserRouter>
